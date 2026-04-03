088befbf58e18d6a8063fd3a7efdddbd461872c8
qdung030404 <qdung030404@gmail.com>
2026-04-04 00:08:07 +0700

    Establish a Supabase connection and configure environment variables using Dotenv.

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+void main() async {
+  await dotenv.load(fileName: ".env");
+  WidgetsFlutterBinding.ensureInitialized();
 
-void main() {
+  await Supabase.initialize(
+    url: dotenv.env['SUPABASE_URL']!,
+    anonKey: dotenv.env['SUPABASE_ANON_KEY']!,
+  );
   runApp(const MyApp());
 }
 
