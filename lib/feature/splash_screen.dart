e8847c88d85969af0e53398714f37783b19ae232
qdung030404 <qdung030404@gmail.com>
2026-04-06 23:43:16 +0700

    feat: add splash screen

---
+++ lib/feature/splash_screen.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:quiz_app/feature/intro/intro.dart';
+import 'package:quiz_app/main.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  final supabase = Supabase.instance.client;
+
+  Future<void> nextScreen() async {
+    await Future.delayed(Duration(seconds: 3));
+    if (supabase.auth.currentSession == null) {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => Intro()),
+      );
+    } else {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => MyHomePage(title: 'home')),
+      );
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    nextScreen();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.deepPurpleAccent,
+      body: Center(child: FlutterLogo(size: 100)),
+    );
+  }
+}
