780f66171498f85b1dc85fd58a788ee68cdc177d
qdung030404 <qdung030404@gmail.com>
2026-04-13 23:30:58 +0700

    feat: implement folder creation logic and UI.

--- lib/feature/folder/view/create_folder_screen.dart
+++ lib/feature/folder/view/create_folder_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get_core/src/get_main.dart';
 import 'package:get/get_navigation/src/extension_navigation.dart';
+import 'package:get/get_state_manager/src/rx_flutter/rx_obx_widget.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:quiz_app/core/widgets/base_screen.dart';
 import 'package:quiz_app/feature/folder/controller/create_folder_controller.dart';
